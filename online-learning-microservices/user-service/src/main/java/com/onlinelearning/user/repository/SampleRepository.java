1a02a375a5e89196974d3329bb1117a5b2f99354
MDeekshitha <yourgithubemail@example.com>
2025-08-18 10:06:28 +0530

    Delete online-learning-microservices folder

--- online-learning-microservices/user-service/src/main/java/com/onlinelearning/user/repository/SampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlinelearning.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.onlinelearning.user.entity.SampleEntity;
-
-public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
-}
