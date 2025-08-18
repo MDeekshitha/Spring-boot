cd3ec7fb2e5b2e7a56e357b86ed9174fa5b0bd2c
Deekshitha <110487564+MDeekshitha@users.noreply.github.com>
2025-08-18 10:04:10 +0530

    Delete online-learning-microservices/assessment-service/src/main/java/com/onlinelearning/assessment directory

--- online-learning-microservices/assessment-service/src/main/java/com/onlinelearning/assessment/repository/SampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlinelearning.assessment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.onlinelearning.assessment.entity.SampleEntity;
-
-public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
-}
